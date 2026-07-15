36efc7a8d4a7423b0db49ebaa8702b5d0f192982
MohamedFazil1406 <mohamedfazil01406@gmail.com>
2026-07-15 15:44:21 +0530

    port number change

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -20,7 +20,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5174"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
